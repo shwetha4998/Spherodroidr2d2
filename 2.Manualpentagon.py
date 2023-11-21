8957a986be57f92006e18350cec771834d0abef1
shwetha4998 <67305794+shwetha4998@users.noreply.github.com>
2023-11-21 14:36:43 +0000

    Update 2.Manualpentagon.py

--- 2.Manualpentagon.py
+++ 2.Manualpentagon.py
@@ -38,26 +38,4 @@ def drive_pentagon(droid: SpheroEduAPI, n: int, speed: int = 50, duration: int =
 with SpheroEduAPI(toy) as droid:
        drive_pentagon(droid,5 ,100,1)   
 
-'''    angle = 360 / n
-    turn_angle = round(angle / 2)
-    turn_duration = round((turn_angle / 360) * duration)
-    
-    for _ in range(n):
-        droid.roll( turn_angle,speed,duration)
-        droid.roll(360 - (2 * turn_angle),speed ,duration)
-        droid.sleep(turn_duration)'''
 
-# Usage
-# Connect to the robot using SpheroEduAPI
-#droid = SpheroEduAPI(toy)
-
-# Drive in a pentagon
-  # Stop the robot after completing the polygon
-'''if __name__ == '__main__':
-    with SpheroEduAPI(toy) as droid:
-       drive_pentagon(droid, 5)'''
-# Example usage:
-# Replace 'your_droid_instance' with your actual SpheroEduAPI instance
-# Replace '5' with the number of sides for a pentagon
-# You can adjust the 'speed' and 'duration' parameters as needed
-# drive_polygon(your_droid_instance, 5, speed=50, duration=1.0)
